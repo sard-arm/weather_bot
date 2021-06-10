58d70dc58df3dae89cc0e206ec0faa694e2f0ab0
sard-arm <armansardaryan208@gmail.com>
2021-06-10 22:26:52 +0300

    1.0v

--- weather_bot.py
+++ weather_bot.py
@@ -1,8 +1,20 @@
 import telebot
 import random
-
 API_TOKEN = '1743525616:AAHKI17GeI8GZp5uti48wq4eLCGGGHW31X0'
 bot = telebot.TeleBot(API_TOKEN)
+@bot.message_handler(commands=['start'])
+def start(message):
+    answer = "Hello"
+    bot.send_message(chat_id=message.chat.id, text=answer)
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    answer = "13 *c"
+    if message.text == "moscow":
+        bot.send_message(message.chat.id, answer)
+
+  
+
+#dsdascscsccca
 
 
 
@@ -16,28 +28,6 @@ bot = telebot.TeleBot(API_TOKEN)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
 bot.polling()
+
+
