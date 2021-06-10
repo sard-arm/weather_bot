9a02a8bff28cefdb65b3f667992d7f4a096ea15e
sard-arm <68128747+sard-arm@users.noreply.github.com>
2021-06-10 20:30:37 +0300

    Osnova

---
+++ weather_bot.py
@@ -0,0 +1,43 @@
+import telebot
+import random
+
+API_TOKEN = '1743525616:AAHKI17GeI8GZp5uti48wq4eLCGGGHW31X0'
+bot = telebot.TeleBot(API_TOKEN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+bot.polling()
